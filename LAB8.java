0a36cf1fbc6ea08f73c09eaf2ad136ed5332e4f8
eliejoefarah <99485801+eliejoefarah@users.noreply.github.com>
2022-04-08 17:40:55 +0300

    Add files via upload

---
+++ LAB8.java
@@ -0,0 +1,53 @@
+package Files;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+
+public class LAB8 {
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
+		FileWriter file = new FileWriter("expenses.txt",true);
+		String redo;
+		
+		do {
+			System.out.println("Input your name: ");
+			String name = scan.nextLine();
+			
+			System.out.println("What did you purchase? ");
+			String purchase = scan.nextLine();
+			
+			System.out.println("How much did you pay? (in USD) ");
+			double price = scan.nextDouble();
+			
+			String str = name + " purchased " + purchase + " for " + price + " US Dollars.";
+			
+			file.write(str + "\n");
+			
+			System.out.print("Would you like to log another purchase? (y/n) ");
+			redo = scan.nextLine();
+			
+		}
+		
+		while (redo.equalsIgnoreCase("y"));
+		
+		file.close();
+		
+		System.out.print("Would you like to read a summary of your purchases? ");
+		String summ = scan.next();
+		
+		File newFile = new File("expenses.txt");
+		Scanner scanFromFile = new Scanner(newFile);	
+		
+		if (summ.equalsIgnoreCase("y")) {
+			while(scanFromFile.hasNext()) {
+				String summary = scanFromFile.nextLine();
+				System.out.print(summary);
+				
+			}
+			
+		}
+		
+	}
+		
+}
